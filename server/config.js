1599f1f2f27f8ded4c955d464dcba988f4d9459b
ethanbudreau <ethanbudreau@gmail.com>
2018-12-01 17:38:29 -0600

    First commit

---
+++ server/config.js
@@ -0,0 +1,7 @@
+const config = {
+  //mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://testuser:testuser1@ds037987.mlab.com:37987/mclaren',
+  port: process.env.PORT || 8000,
+};
+
+export default config;
