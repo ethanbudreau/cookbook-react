c944d6be4e63c0c1b2ca064ee71c08079059250a
Ethan Budreau <ethanbudreau@gmail.com>
2018-12-01 21:32:41 -0600

    package-lock update and config comment space added

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  //mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  // mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
   mongoURL: process.env.MONGO_URL || 'mongodb://testuser:testuser1@ds037987.mlab.com:37987/mclaren',
   port: process.env.PORT || 8000,
 };
